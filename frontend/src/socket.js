e37fc8adefa4b102905068ffdff502a1f86d83ba
Matek123-max <mfraczek08@outlook.com>
2025-07-23 17:50:33 +0200

    Update socket.js

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,3 +1,3 @@
 import { io } from "socket.io-client";
-const socket = io("http://localhost:4000"); // jeśli backend na innym adresie, wpisz ten adres
+const socket = io("https://czolko-3.onrender.com"); // <-- tu twój backend!
 export default socket;
